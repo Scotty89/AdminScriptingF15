278cae9a0d11061e3d09d80f1f75bc513e8497b2
Scotty89 <cadieux.scott@gmail.com>
2015-09-29 14:56:25 -0400

    Update find-setid.sh

--- find-setid.sh
+++ find-setid.sh
@@ -2,5 +2,13 @@
 # this scirpt lists all the setuid and setgid files in /usr
 # the files are displayed using ls -l and sorted by user and group
 
+echo
+echo "These files are set-uid"
+echo "======================="
 find /usr -type f -perm -4000 -exec ls -l {} \; | sort -k 3
+
+echo
+echo "These files are set-gid"
+echo "======================="
 find /usr -type f -perm -2000 -exec ls -l {} \; | sort -k 4
+echo
